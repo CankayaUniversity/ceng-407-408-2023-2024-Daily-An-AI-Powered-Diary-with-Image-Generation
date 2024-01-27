9aad7f47710be07555a41d8d5e8c6de486ecbc29
Ahmet Yaka <yakaahmet23@gmail.com>
2024-01-27 23:31:09 +0300

    model fixes & sample functions

--- handler/handler.go
+++ handler/handler.go
@@ -54,3 +54,35 @@ func GetUsers(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, users)
 }
+
+func CreateDaily(c *gin.Context) {
+	var daily model.Daily
+	err := c.ShouldBindJSON(&daily)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	daily.ID = primitive.NewObjectID()
+	daily.CreatedAt = primitive.NewDateTimeFromTime(time.Now())
+	_, err = database.Dailies.InsertOne(c, daily)
+	if err != nil {
+		c.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+func GetDailies(c *gin.Context) {
+	var dailies []model.Daily
+	cursor, err := database.Dailies.Find(c, bson.D{})
+	if err != nil {
+		c.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	err = cursor.All(c, &dailies)
+	if err != nil {
+		c.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, dailies)
+}
