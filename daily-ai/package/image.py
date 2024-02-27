1d0537771c6a5e90d486e45d4bd11cb98e783f93
FBCruel <mmertturkmen@gmail.com>
2024-02-27 16:14:39 +0300

    ai server packaged

---
+++ daily-ai/package/image.py
@@ -0,0 +1,14 @@
+import base64
+
+def getImage():
+    base64Image = None
+    try:
+        imagePath = "./venv/Image/sampleImage.jpg"
+        with open(imagePath, "rb") as imgFile:
+            img = imgFile.read()
+            base64Image = base64.b64encode(img).decode('utf-8')
+        print("Image loaded and converted to base64 succesfully.")
+    except FileNotFoundError:
+        print("Image file not found at path: ", imagePath)
+
+    return "base64Image"
