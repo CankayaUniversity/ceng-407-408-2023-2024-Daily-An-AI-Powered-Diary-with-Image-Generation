19b408ff26831790cbc2d9fc04126be2ac12d8e7
FBCruel <mmertturkmen@gmail.com>
2024-02-29 00:24:23 +0300

    README.md added

---
+++ daily-ai/README.md
@@ -0,0 +1,7 @@
+To run this app
+
+1) python -m venv venv
+2) .\venv\Scripts\activate
+3) python -m pip install --upgrade pip
+4) python -m pip install flask
+5) python -m flask --app .\app.py run
--- daily-ai/app.py
+++ daily-ai/app.py
@@ -3,12 +3,6 @@ from package import responseWrapper
 
 app = Flask(__name__)
 
-"""To run this app 
-1) python -m venv venv
-2) .\venv\Scripts\activate
-3) python -m pip install --upgrade pip
-4) python -m pip install flask
-5) python -m flask --app .\app.py run"""
 @app.route("/")
 
 def main():
