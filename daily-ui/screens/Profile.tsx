db73730b7f032ed1570e9b614454f8c94433ff56
metshah <umitmetesahin@gmail.com>
2024-03-18 18:46:54 +0300

    profile in progress

--- daily-ui/screens/Profile.tsx
+++ daily-ui/screens/Profile.tsx
@@ -6,20 +6,36 @@ const Profile = ({navigation}:{navigation:any}) => {
    return (
       <Header navigation={navigation} previous="Home" homepage={true}>
             <View style={styles.container}>
-               <View style={styles.columns}>
+               <View style={styles.container2}>
                   <View style={styles.rows}>
-                     <View style={{width:'40%', height:'40%', paddingTop:12,borderRadius:12, alignItems: 'center',backgroundColor:'rgba(0,0,0,0.4)',aspectRatio: 1/1}}>
+                     <View style={{width:'40%', paddingTop:12,borderRadius:12, alignItems: 'center',backgroundColor:'rgba(0,0,0,0.4)',aspectRatio: 1/1}}>
                         <View style={styles.avatarPlaceholder}/>
                      </View>
-                     <View style={{width:'60%',height:'40%', paddingTop:12,borderRadius:12,backgroundColor:'rgba(0,0,0,0.4)'}}>
+                     <View style={{width:'60%', paddingTop:12,borderRadius:12,backgroundColor:'rgba(0,0,0,0.4)'}}>
                         <Text style={styles.name}>daily dev</Text>
                         <Text style={styles.username}>@daily_dev</Text>
                      </View>
                   </View>
                   <View style={styles.rows}>
-                     <View style={{width:'40%', height:'20%', paddingTop:12,borderRadius:12,backgroundColor:'rgba(0,0,0,0.4)'}}>
+                     <View style={styles.box2}>
                         <Text style={styles.username}>test</Text>
-                     </View> 
+                     </View>  
+                  </View>
+                  <View style={styles.rows}>
+                     <View style={styles.box1}>
+                        <Text style={styles.username}>test</Text>
+                     </View>
+                     <View style={styles.box1}>
+                        <Text style={styles.username}>test</Text>
+                     </View>  
+                  </View>
+                  <View style={styles.rows}>
+                     <View style={styles.box1}>
+                        <Text style={styles.username}>test</Text>
+                     </View>
+                     <View style={styles.box1}>
+                        <Text style={styles.username}>test</Text>
+                     </View>  
                   </View>
                   
                </View>
@@ -30,36 +46,47 @@ const Profile = ({navigation}:{navigation:any}) => {
 
 const styles = StyleSheet.create({
    container: {
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'flex-start',
+      height: '90%',
       padding:12
    },
-   columns: {
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'flex-start',
+   container2: {
+      height: '100%',
       borderRadius:12,
       padding:12,
       backgroundColor:'rgba(0,0,0,0.4)'
    },
    rows: {
+      justifyContent: 'space-around',
+      height: '25%',
+      marginBottom: 12,
       flex: 1,
       flexDirection: 'row',
-      justifyContent: 'flex-start',
       borderRadius:12,
    },
-   box: {
+   box1: {
       paddingStart:12,
       paddingEnd:12,
       paddingTop:12, 
       borderTopStartRadius:12,
-      borderTopEndRadius:12
+      borderTopEndRadius:12,
+      width:'50%', 
+      borderRadius:12,
+      backgroundColor:'rgba(0,0,0,0.4)' 
+   },
+   box2: {
+      paddingStart:12,
+      paddingEnd:12,
+      paddingTop:12, 
+      borderTopStartRadius:12,
+      borderTopEndRadius:12,
+      width:'100%', 
+      borderRadius:12,
+      backgroundColor:'rgba(0,0,0,0.4)'
    },
    name: {
       textAlign:'right',
       marginEnd:12.5,
-      fontSize:50,
+      fontSize:36,
       fontWeight:'500',
       color:'white',
       opacity: 1.0
@@ -67,7 +94,7 @@ const styles = StyleSheet.create({
    username: {
       textAlign:'right',
       marginEnd:12.5,
-      fontSize:20,
+      fontSize:18,
       fontWeight:'200',
       color:'white',
       opacity: 0.5
@@ -78,8 +105,8 @@ const styles = StyleSheet.create({
       borderRadius: 50,
    },
    avatarPlaceholder: {
-      width: '80%',
-      height: '80%',
+      width: 128,
+      height: 128,
       borderRadius: 100,
       backgroundColor: '#FFFFFF',
       opacity: 0.5,
