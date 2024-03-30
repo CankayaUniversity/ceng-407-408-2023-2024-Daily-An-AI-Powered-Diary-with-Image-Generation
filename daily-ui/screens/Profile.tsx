dd6af00e9bd5fdf2b1a021a7a4bdce08b4440fd0
metshah <umitmetesahin@gmail.com>
2024-03-30 23:01:30 +0300

    test

--- daily-ui/screens/Profile.tsx
+++ daily-ui/screens/Profile.tsx
@@ -1,43 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
+import { BlurView } from "expo-blur";
 import { StyleSheet, Text, View,Alert,TouchableOpacity, ImageBackground } from 'react-native';
 import Header from '../components/Header';
 
 const Profile = ({navigation}:{navigation:any}) => {
    return (
       <Header navigation={navigation} previous="Home" homepage={true}>
-            <View style={styles.container}>
-               <View style={styles.container2}>
-                  <View style={styles.rows}>
-                     <View style={styles.box1}>
-                        <View style={styles.avatarPlaceholder}/>
-                           <Text style={styles.name}>daily dev</Text>
-                           <Text style={styles.username}>@daily_dev</Text>
-                     </View>
+         <View style={styles.container}>
+            <BlurView style={styles.container2} tint="dark" intensity={70}>
+               <View style={styles.rows}>
+                  <View style={styles.avatarbox}>
+                     <View style={styles.avatarPlaceholder} />
                   </View>
-                  <View style={styles.rows}>
-                     <View style={styles.box1}>
-                        <Text style={styles.username}>test</Text>
-                     </View>  
+                  <View>
+                     <Text style={styles.name}>umit mete sahin</Text>
+                     <Text style={styles.username}>@daily_dev</Text>
                   </View>
-                  <View style={styles.rows}>
-                     <View style={styles.box1}>
-                        <Text style={styles.username}>test</Text>
-                     </View>
-                     <View style={styles.box1}>
-                        <Text style={styles.username}>test</Text>
-                     </View>  
-                  </View>
-                  <View style={styles.rows}>
-                     <View style={styles.box1}>
-                        <Text style={styles.username}>test</Text>
-                     </View>
-                     <View style={styles.box1}>
-                        <Text style={styles.username}>test</Text>
-                     </View>  
-                  </View>
-                  
                </View>
-            </View>
+               <View style={styles.rows}>
+                  <View style={[styles.box1, { flexDirection: 'row' }]}>
+                     <View style={styles.subbox}></View>
+                     <View style={styles.subbox}>
+                        <Text style={styles.title}>You are on a streak!</Text>
+                        <Text style={styles.subtitle}>Keep up your streak to further enhance your public daily interaction amount</Text>
+                     </View>
+                  </View>
+               </View>
+               <View style={styles.rows}>
+                  <View style={styles.box1}>
+                     <View style={styles.subbox}>
+                        <Text style={[styles.title, {textAlign: 'left'}]}>Your dailies are being seen!</Text>
+                        <Text style={[styles.subtitle, {textAlign: 'left'}]}>keep up the good work!</Text>
+                     </View>
+                     <View style={styles.subbox}>
+                       
+                     </View>
+                  </View>
+                  <View style={styles.box1}>
+                     <Text style={[styles.title, {textAlign: 'left'}]}>This week, you are feeling...</Text>
+                  </View>
+               </View>
+               <View style={styles.rows}>
+                  <View style={styles.box1}>
+                     <Text style={styles.username}>test</Text>
+                  </View>
+                  <View style={styles.box1}>
+                     <Text style={styles.username}>test</Text>
+                  </View>
+               </View>
+
+            </BlurView>
+         </View>
       </Header>
    );
 }
@@ -66,6 +79,22 @@ const styles = StyleSheet.create({
       borderRadius: 12,
       alignItems: 'stretch',
    },
+   title: {
+      textAlign:'right',
+      marginEnd:12.5,
+      fontSize:18,
+      fontWeight:'600',
+      color:'white',
+      opacity: 1.0
+   },
+   subtitle: {
+      textAlign:'right',
+      marginEnd:12.5,
+      fontSize:14,
+      fontWeight:'500',
+      color:'white',
+      opacity: 0.4
+   },
    box1: {
       flex: 3,
       height: '100%',
@@ -77,6 +106,24 @@ const styles = StyleSheet.create({
       borderRadius:12,
       alignSelf: 'flex-start',
       backgroundColor:'rgba(0,0,0,0.4)' 
+   },
+   subbox: {
+      flex: 3,
+      height: '100%',
+      alignSelf: 'flex-start',
+      backgroundColor:'rgba(0,0,0,0)' 
+   },
+   avatarbox: {
+      flex: 1,
+      height: '100%',
+      paddingStart:12,
+      paddingEnd:12,
+      paddingTop:12,
+      borderTopStartRadius:12,
+      borderTopEndRadius:12,
+      borderRadius:12,
+      alignSelf: 'flex-start',
+      backgroundColor:'rgba(0,0,0,0)' 
    },
    name: {
       textAlign:'right',
