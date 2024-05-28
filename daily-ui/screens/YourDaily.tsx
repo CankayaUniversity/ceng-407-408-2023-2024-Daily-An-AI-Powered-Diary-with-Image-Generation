2682860939f5b5fb8c5977c67a78db88bd79c7fc
ahmetyaka <yakaahmet23@gmail.com>
2024-05-28 17:02:47 +0300

    improvements

--- daily-ui/screens/YourDaily.tsx
+++ daily-ui/screens/YourDaily.tsx
@@ -1,29 +1,62 @@
-import React from 'react';
-import { ScrollView, Pressable, Text, Image,View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, Pressable, Text, Image,View, Alert} from 'react-native';
 import Header from '../components/Header';
-import { useGetDailies } from '../libs';
+import { EditDailyImageRequest, useEditDailyImage, useGetDailies } from '../libs';
+import * as ImagePicker from 'expo-image-picker';
 
 const YourDaily = ({ navigation }: { navigation: any }) => {
-  const { isError, isLoading, data } = useGetDailies(10);
+  const {isLoading, data,isRefetching } = useGetDailies();
+  const {isPending,mutate} = useEditDailyImage();
   const dateDict = (dateStr:string) => {
     const date = new Date(dateStr);
     const monthArr = ["JAN",	"FEB",	"MAR",	"APR",	"MAY",	"JUN",	"JUL",	"AUG",	"SEPT",	"OCT",	"NOV",	"DEC"]
 
-    return monthArr[date.getMonth()+1]+"\n"+date.getDay();
+    return monthArr[date.getMonth()]+"\n"+date.getDate();
   };
+
+  const pickImage = async (id:string) => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+      base64:true
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      const editReq: EditDailyImageRequest ={
+        id:id,
+        image:'data:image/jpeg;base64,'+result.assets[0].base64 as string
+      }
+      mutate(editReq)
+    }
+  };
+
 
   return (
     <Header navigation={navigation} previous="Home" homepage={false}>
       <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ flexDirection: "row", flexWrap: 'wrap', justifyContent: 'flex-start', gap: 10, alignItems: 'center', marginTop: 10, paddingStart: 15, paddingBottom: 90 }}>
         {
-          isLoading &&
+          (isLoading || isPending) &&
           <Text style={{ alignItems: 'center', justifyContent: 'center', fontSize: 40, color: 'white' }}>Loading</Text>
         }
-        {data?.map((el, index) => {
+        {(!isLoading && !isPending) && data?.map((el, index) => {
           return (
             <Pressable
             key={index}
             onPress={() => navigation.navigate("ReadDaily", { data: el })}
+            onLongPress={()=>Alert.alert(
+              '',
+              'Do you want to change the image?',  
+              [
+                 {text: 'Yes', onPress: () => pickImage(el.id), style: 'cancel'},
+                 {text: 'No'},
+              ],
+              { cancelable: false }
+         )}
             style={{
               aspectRatio: 1,
               width: '30%',
@@ -31,7 +64,6 @@ const YourDaily = ({ navigation }: { navigation: any }) => {
               borderColor: 'gray',
               alignItems: "center",
               justifyContent: "center",
-              opacity: 0.85,
               borderRadius: 10,
               backgroundColor: '#0D1326',
             }}
