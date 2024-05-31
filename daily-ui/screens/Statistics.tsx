74a90c7eb4eec9995d3169511c96a9d7ed1898e7
ahmetyaka <yakaahmet23@gmail.com>
2024-05-31 17:49:38 +0300

    fix

--- daily-ui/screens/Statistics.tsx
+++ daily-ui/screens/Statistics.tsx
@@ -117,7 +117,7 @@ const styles = StyleSheet.create({
     outerRow: {
         marginTop: "6%",
         width: '92%',
-        height:'15%',
+        height:100,
         flexDirection: 'row',
         justifyContent: 'space-between',
     },
